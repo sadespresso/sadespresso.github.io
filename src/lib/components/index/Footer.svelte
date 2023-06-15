c62ddeb7e89f1b573f732dcfd5e91e22aadc60fc
theironbatka <batmend22@gmail.com>
2023-06-16 00:40:00 +0800

    add space

--- src/lib/components/index/Footer.svelte
+++ src/lib/components/index/Footer.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import LL from '$lib/i18n/i18n-svelte';
+	import Spacer from '../Spacer.svelte';
 </script>
 
+<Spacer />
 <p class="expand-width center">
 	<b>
 		{$LL.footer.rights()}
 	</b>
 </p>
+<Spacer />
